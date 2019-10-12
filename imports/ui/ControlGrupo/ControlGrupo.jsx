e81f8434b3ad0b81cac99fcd2a3768a0dceaf620
mateo <m.devia@uniandes.edu.co>
2019-10-12 15:48:02 -0500

    modals working again

--- imports/ui/ControlGrupo/ControlGrupo.jsx
+++ imports/ui/ControlGrupo/ControlGrupo.jsx
@@ -1,19 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 import "./ControlGrupo.css";
+import ModalAgregarIntegrante from "../ModalAgregarIntegrante/ModalAgregarIntegrante";
+import ModalAgregarTarea from "../ModalAgregarTarea/ModalAgregarTarea";
 
-class ControlGroup extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <React.Fragment>
-        <button className="botonControl">Agregar Integrate</button>
-        <button className="botonControl">Agregar Tera</button>
-      </React.Fragment>
-    );
-  }
+function ControlGroup() {
+  const [modalShow, setModalShow] = React.useState(false);
+  const [modalShow2, setModalShow2] = React.useState(false);
+  return (
+    <React.Fragment>
+      <button className="botonControl" onClick={() => setModalShow(true)}>
+        Agregar Integrate
+      </button>
+      <button className="botonControl" onClick={() => setModalShow2(true)}>
+        Agregar Tarea
+      </button>
+      <ModalAgregarIntegrante
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+      <ModalAgregarTarea
+        show={modalShow2}
+        onHide={() => setModalShow2(false)}
+      />
+    </React.Fragment>
+  );
 }
 
 export default ControlGroup;
